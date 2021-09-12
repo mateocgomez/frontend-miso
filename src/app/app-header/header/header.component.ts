b8d79d00d8e739d3cb8aeeeb20269e75b058d05b
mateodev <gomezmateo94@gmail.com>
2021-09-12 10:48:28 -0500

    Correcion rutas

--- src/app/app-header/header/header.component.ts
+++ src/app/app-header/header/header.component.ts
@@ -4,40 +4,43 @@ import { Router, ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
   languageList = [
     { code: 'en', label: 'English' },
-    { code: 'es', label: 'Espanol' }
+    { code: 'es', label: 'Espanol' },
   ];
 
-  constructor(
-    private routerPath: Router,
-    private router: ActivatedRoute
-    ) { }
+  constructor(private routerPath: Router, private router: ActivatedRoute) {}
 
-  ngOnInit(): void {  }
+  ngOnInit(): void {}
 
-  goTo(menu: string){
-    const userId = parseInt(this.router.snapshot.params.userId)
-    const token = this.router.snapshot.params.userToken
+  goTo(menu: string) {
+    const userId = parseInt(this.router.snapshot.params.userId);
+    const token = this.router.snapshot.params.userToken;
 
     switch (menu) {
-      case "logIn":
-        this.routerPath.navigate([`/`])
+      case 'logIn':
+        this.routerPath.navigate([`/`]);
         break;
-      case "album":
-        this.routerPath.navigate([`/albumes/${userId}/${token}`])
+      case 'album':
+        this.routerPath.navigate([`/albumes/${userId}/${token}`]);
         break;
-      case "cancion":
-        this.routerPath.navigate([`/canciones/${userId}/${token}`])
+      case 'cancion':
+        this.routerPath.navigate([`/canciones/${userId}/${token}`]);
         break;
-      case "acerca":
-        this.routerPath.navigate([`/acerca_de/${userId}/${token}`])
+      case 'acerca':
+        this.routerPath.navigate([`/acerca_de/${userId}/${token}`]);
         break;
       default:
-        this.routerPath.navigate([menu])
+        this.routerPath.navigate([menu]);
     }
+  }
+
+  switchLanguage(languageCode: string) {
+    const url = `/${languageCode}${this.routerPath.url}`;
+    console.log(url);
+    location.href = url;
   }
 }
